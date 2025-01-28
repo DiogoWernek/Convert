90d3566a43e8ec9dd94c55b3c021e172065728d3
DiogoWernek <diogowernek86@gmail.com>
2025-01-27 23:13:31 -0300

    Formatando o resultado

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // Cotação de moedas do dia.
-const USD = 6;
-const EUR = 7;
-const GBP = 8;
+const USD = 6.12;
+const EUR = 7.22;
+const GBP = 8.13;
 
 // Obtendo elementos para serem trabalhados.
 const main = document.querySelector("main");
@@ -9,6 +9,8 @@ const form = document.querySelector("form");
 const amount = document.querySelector("#amount");
 const currency = document.querySelector("#currency");
 const footer = document.querySelector("main footer");
+const description = document.querySelector("#description");
+const result = document.querySelector("#result");
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -38,6 +40,23 @@ form.onsubmit = (event) => {
 // Função para converter a moeda.
 function convertCurrency(amount, price, symbol) {
   try {
+    // Atualizando o conteúdo e exibindo a cotação da moeda selecionada.
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+
+    // Calcula o total.
+    let total = amount * price;
+
+    // Formatar o total para Reais com vírgula
+    total = formatCurrencyBRL(total).replace("R$", "");
+    
+    // Exibir o Resultado total.
+    result.textContent = `${total} Reais`;
+
+    // Verificar se é ou não um número.
+    if(isNaN(total)) {
+      return alert("Por favor, digite o valor corretamente para converter.")
+    }
+
     // Aplica a classe que exibe o footer para mostrar o resultado.
     footer.classList.add("show-result");
   } catch (error) {
@@ -46,5 +65,12 @@ function convertCurrency(amount, price, symbol) {
     alert("Não foi possível converter, tente novamente mais tarde.");
     console.log(error);
   }
+}
 
+// Função para formatar a moeda para o real brasileiro.
+function formatCurrencyBRL(value) {
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  })
 }
